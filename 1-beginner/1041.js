561ca72c019996df785019c69c74d5c685903ee0
ANDRESSA <andressajucoski@gmail.com>
2020-09-23 21:43:46 -0300

    1041.js

---
+++ 1-beginner/1041.js
@@ -0,0 +1,35 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+values = lines.shift().split(' ');
+
+x = parseFloat(values.shift());
+y = parseFloat(values.shift());
+
+if (x > 0 && y > 0) {
+    console.log('Q1');
+}
+
+else if (x < 0 && y > 0) {
+    console.log('Q2');
+}
+
+else if (x < 0 && y < 0) {
+    console.log('Q3');
+}
+
+else if (x > 0 && y < 0) {
+    console.log('Q4');
+}
+
+else if (x !== 0 && y === 0) {
+    console.log('Eixo X');
+}
+
+else if (x === 0 && y !== 0) {
+    console.log('Eixo Y');
+}
+
+else {
+    console.log('Origem');   
+}
